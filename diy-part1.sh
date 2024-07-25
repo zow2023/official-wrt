cea5ad325824d4272d9998757fe7dfabc0e33bd3
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-07-25 11:50:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,7 @@
 # See /LICENSE for more information.
 #
 
-#git checkout ebb3faf31f7c34d71e8ffbf3e8d94bf92086188e
+git checkout ebb3faf31f7c34d71e8ffbf3e8d94bf92086188e
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -18,3 +18,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
