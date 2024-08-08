c4948ddf6308eda317f7524529e9e5077bfd971b
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-08-08 12:16:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,7 +37,7 @@ git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-pa
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 #git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
-git clone https://github.com/QiuSimons/luci-app-daed-next package/daed-next
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
 
 #git clone https://github.com/sbwml/v2ray-geodata package/daed/v2ray-geodata
 #git clone https://github.com/QiuSimons/luci-app-daed package/dae
