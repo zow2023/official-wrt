736778d2014678bcc72fee35c5c037404dbf222d
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-08-08 20:24:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,12 +24,12 @@
 #git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
 #git clone https://github.com/DevOpenWRT-Router/luci-app-mqos package/luci-app-mqos
 
-#git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
+git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package     
 
 git clone https://github.com/CrazyPegasus/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
 git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
 #git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
-#git clone https://github.com/sbwml/autocore-arm -b openwrt-22.03 package/autocore-arm
+#git clone https://github.com/sbwml/autocore-arm -b openwrt-22.03 package/autocore-arm 
 #git clone https://github.com/gSpotx2f/luci-app-cpu-status package/luci-app-cpu-status
 #git clone https://github.com/gSpotx2f/luci-app-cpu-perf package/luci-app-cpu-perf
 #git clone https://github.com/gSpotx2f/luci-app-interfaces-statistics package/luci-app-interfaces-statistics
@@ -37,13 +37,13 @@ git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-pa
 #git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
 #git clone https://github.com/muink/luci-app-tn-netports package/luci-app-tn-netports
 
-git clone https://github.com/QiuSimons/luci-app-daed package/dae
+git clone https://github.com/QiuSimons/luci-app-daed-next package/daed-next
 
-#git clone https://github.com/sbwml/v2ray-geodata package/daed/v2ray-geodata
-#git clone https://github.com/QiuSimons/luci-app-daed package/dae
+#git clone https://github.com/sbwml/v2ray-geodata package/daed/v2ray-geodata 
+#git clone https://github.com/QiuSimons/luci-app-daed package/dae     
 
-#rm -rf feeds/packages/net/xray-core
-#rm -rf feeds/packages/net/chinadns-ng
+#rm -rf feeds/packages/net/xray-core  
+#rm -rf feeds/packages/net/chinadns-ng 
 #rm -rf feeds/luci/applications/luci-app-passwall
 
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci
@@ -57,10 +57,10 @@ git clone https://github.com/QiuSimons/luci-app-daed package/dae
 #rm -rf feeds/packages/utils/v2dat
 #rm -rf feeds/packages/net/adguardhome
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
-#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f 
 #find ./ | grep Makefile | grep mosdns | xargs rm -f
 
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
